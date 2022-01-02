a694dbd03dba38f3f39d24376929340e22b56521
MeloDev <melodiaz2111@gmail.com>
2022-01-01 19:32:49 -0500

    Update Orden Controller

--- src/main/java/com/sistemas/monolito/controlador/OrdenController.java
+++ src/main/java/com/sistemas/monolito/controlador/OrdenController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -52,7 +53,8 @@ public class OrdenController {
     public String postOrdenFormNew(
             @Valid @ModelAttribute("orden") Orden orden,
             BindingResult bindingResult,
-            Model model){
+            Model model,
+            RedirectAttributes redirectAttributes){
 
         List<Asignacion> asignaciones = new ArrayList<Asignacion>();
 
@@ -73,7 +75,7 @@ public class OrdenController {
 
         orden.setAsignaciones(asignaciones);
         ordenService.agregar(orden);
-
+        redirectAttributes.addFlashAttribute("flash", "Agregado Correctamente");
         return "redirect:/orden/index";
     }
 
@@ -81,12 +83,8 @@ public class OrdenController {
     public String getOrdenFormEdit(@PathVariable("id") Long id,
                                    Model model){
         Optional<Orden> buscado = ordenService.buscar(id);
-
-        if(buscado.isPresent()){
-            model.addAttribute("orden",buscado.get());
-        }else{
-            model.addAttribute("orden", new Orden());
-        }
+        model.addAttribute("orden",
+                buscado.isPresent() ? buscado.get() : new Orden());
 
         model.addAttribute("listaClientes", clienteService.listarTodos());
         model.addAttribute("listaTarifas", tarifaService.listarTodos());
@@ -98,7 +96,8 @@ public class OrdenController {
     private String postOrdenFormEdit(
             @Valid @ModelAttribute("orden") Orden orden,
             BindingResult bindingResult,
-            Model model){
+            Model model,
+            RedirectAttributes redirectAttributes){
 
         if(bindingResult.hasErrors()){
             model.addAttribute("listaClientes", clienteService.listarTodos());
@@ -106,17 +105,17 @@ public class OrdenController {
 
             return "orden/ordenForm";
         }
-
+        //TODO Revisar si es agregar o actualizar el metodo
         ordenService.agregar(orden);
-
+        redirectAttributes.addFlashAttribute("flash", "Modificado Correctamente");
         return "redirect:/orden/index";
     }
 
     @GetMapping("/eliminar/{id}")
     private String getOrdenEliminar(@PathVariable("id") Long id,
-                                    Model model){
+                                    RedirectAttributes redirectAttributes){
         ordenService.eliminar(id);
-
+        redirectAttributes.addFlashAttribute("flash", "Eliminado Correctamente");
         return "redirect:/orden/index";
     }
 
